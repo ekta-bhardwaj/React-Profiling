acc9d21cb34adf17ad2ab36e83fb15276c9406c1
Ekta Bhardwaj <ekta.bhardwaj@quovantis.com>
2020-04-09 17:11:06 +0530

    changes

--- src/Button2.js
+++ src/Button2.js
@@ -2,5 +2,5 @@ import React from "react";
 import { UseCountRender } from "./UseCountRender";
 export const Button2 = ({ increment2 }) => {
   UseCountRender();
-  return <button onClick={increment2}>click</button>;
+  return <button onClick={increment2}>Button 2</button>;
 };
